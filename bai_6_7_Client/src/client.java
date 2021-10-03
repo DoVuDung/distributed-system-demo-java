0912a5f023d4c658a5b3822c9441c8b8aabff19c
DoVuDung <vudungoik2016@gmail.com>
2021-10-03 19:14:50 +0700

    update done bai 6.7

--- bai_6_7_Client/src/client.java
+++ bai_6_7_Client/src/client.java
@@ -6,29 +6,40 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Andy
  */
 public class client {
-    public static void main(String[] args) throws MalformedURLException, NotBoundException{
+    
+    public static void main(String[] args) throws MalformedURLException, NotBoundException {
         try {
             Registry regClient = LocateRegistry.getRegistry("localhost", 9999);
             System.out.println("Client đang tìm kiếm đối tượng...");
             bai_6_7 bt = (bai_6_7) regClient.lookup("rmi://localhost/Calculator");
             System.out.println("Hãy nhập vào các số nguyên từ bàn phím:");
-            Scanner sc = new Scanner(System.in);
-            int number = Integer.parseInt(sc.nextLine());
-            String 
-            string.split(";");
+            while (true) {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Hay nhap 4 so nguyen tu ban phim theo dang (a,b,c,d): ");
+                String inputString = input.nextLine();
+                
+                if (inputString.equals("stop")) {
+                    break;
+                }
+                
+                String[] intArr = inputString.split(",");
+                int a = Integer.parseInt(intArr[0]);
+                int b = Integer.parseInt(intArr[1]);
+                int c = Integer.parseInt(intArr[2]);                
+                int d = Integer.parseInt(intArr[3]);
+                System.out.println("S = 25*(a+b) – 6*(3c- 2d), ");
+                System.out.print(bt.tinhTong(a, b, c, d));
+            }
             
         } catch (RemoteException e) {
             System.out.println("Không tìm thấy đối tượng Remote");
