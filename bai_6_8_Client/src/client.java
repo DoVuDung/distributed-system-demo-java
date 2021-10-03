f9ac1cd3fd6460c22094ea93492040f6f0b6f4e6
DoVuDung <vudungoik2016@gmail.com>
2021-10-03 19:40:28 +0700

    them cau b

--- bai_6_8_Client/src/client.java
+++ bai_6_8_Client/src/client.java
@@ -1,10 +1,10 @@
 
-
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Random;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -23,12 +23,32 @@ public class client {
             System.out.println("Client đang tìm kiếm đối tượng...");
             bai_6_8 bt = (bai_6_8) regClient.lookup("rmi://localhost/Calculator");
             int max = 100, min = 20;
-            Random random = new Random();           
+            Random random = new Random();
             int a = random.nextInt(max - min + 1) + min;
             int b = random.nextInt(max - min + 1) + min;
             int c = random.nextInt(max - min + 1) + min;
             bt.giaiPhuongTrinhBac2(a, b, c);
 
+            // cau b
+            System.out.println("Hãy nhập vào các số nguyên từ bàn phím:");
+            while (true) {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Hay nhap 4 so nguyen tu ban phim theo dang (a;b;c;d): ");
+                String inputString = input.nextLine();
+
+                if (inputString.equals("stop")) {
+                    break;
+                }
+
+                String[] intArr = inputString.split(";");
+                int d = Integer.parseInt(intArr[0]);
+                int e = Integer.parseInt(intArr[1]);
+                int f = Integer.parseInt(intArr[2]);
+
+                bt.giaiPhuongTrinhBac2(a, b, c);
+                bt.giaiPhuongTrinhBac2(d, e, f);
+                
+            }
         } catch (RemoteException e) {
             System.out.println("Không tìm thấy đối tượng Remote");
         }
